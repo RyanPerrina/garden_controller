310bbc0c689bd09275effc3879f7d5d9eaa8121d
RyanPerrina <ryan.perrina@studio.unibo.it>
2022-07-27 18:09:50 +0200

    added httpclient end deleted asfsm on sensorboard

--- garden-sensorboard/garden-sensorboard.ino
+++ garden-sensorboard/garden-sensorboard.ino
@@ -2,27 +2,30 @@
 #include "Led.h"
 #include "Photoresistor.h"
 #include "TemperatureSensor.h"
+#include "httpService.h"
 
 // ????????
 #define PIN_TEMP G34
 #define PIN_LED G35
 #define PIN_PHOTO G32
 
+const char* ssid = "Mi10";
+const char* password = "12345678";
+const char *serviceURI = "https://?????????????????????????????????????????????????????????";
 TemperatureSensor *temp;
 Led *led;
 Photoresistor *photo;
+httpService* http;
 
 void setup() {
   temp = new TemperatureSensor(PIN_TEMP);
   led = new Led(PIN_LED);
   photo = new Photoresistor(PIN_PHOTO);
-
+  http = new httpService(ssid, password, service)
+  http->on()
 }
 
 void loop() {
   delay(10000);
-  l = photo -> getLight();
-  t = temp -> getTemperature();
-
-  // facci qualcosa con quelli
+  http->send(temp -> getTemperature(), photo -> getLight())
 }
